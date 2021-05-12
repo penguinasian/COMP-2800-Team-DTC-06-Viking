a8a307ef9bd814980b278c5656456cacc16b7dca
Kevin <kg25000@gmail.com>
2021-05-12 14:37:29 -0700

    Added swipe tracking

--- public/javascripts/routes.js
+++ public/javascripts/routes.js
@@ -1,10 +1,11 @@
-document.getElementById("routeOneOverlay").addEventListener("mouseover", function() {testMouseOver('routeOneInfo');});
-document.getElementById("routeOneOverlay").addEventListener("mouseleave", function() {removeMouseOut('routeOneInfo');});
+document.getElementById("routeOneOverlay").addEventListener("touchstart", function() {testMouseOver('routeOneInfo');});
+document.getElementById("routeOneOverlay").addEventListener("mouseup", function() {removeMouseOut('routeOneInfo');});
 document.getElementById("routeOneOverlay").addEventListener("dblclick", function() {routeDescriptionRedirect('../public/index.html');});
 
 document.getElementById("routesContent").addEventListener('swiped-right', function(e) {
     console.log(e.target); // element that was swiped
     console.log(e.detail); // event data { dir: 'right', xStart: 196, xEnd: 230, yStart: 196, yEnd: 4 }
+    routeDescriptionRedirect('../public/index.html');
   });
 
 function testMouseOver(elementId) {
