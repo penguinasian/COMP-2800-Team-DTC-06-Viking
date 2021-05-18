e7565c63af991fc7e3aa27b1e6721eb7b82679c3
Jonghoon Jang <jayjang0209@gmail.com>
2021-05-18 14:32:33 -0700

    Move parkade_json.json to data

--- upload_firestore/import.js
+++ upload_firestore/import.js
@@ -2,15 +2,21 @@ const firestoreService = require('firestore-export-import');
 const firebaseConfig = require('./config.js');
 const serviceAccount = require('./serviceAccount.json');
 
+const admin = require("firebase-admin");
+
 /* Upload JSON TO Firestore */
 const jsonToFirestore = async () => {
     try{
         console.log('Initializing Firebase');
-        await firestoreService.initializeApp(serviceAccount, firebaseConfig.databaseURL);
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount),
+            databaseURL: "https://viking-eaee3-default-rtdb.firebaseio.com"
+          });
+        // await firestoreService.initializeApp(serviceAccount, "https://viking-eaee3-default-rtdb.firebaseio.com");
         console.log('Firebase Initialized');
 
         /* enter the name of JSON file below */
-        await firestoreService.restore('./parkade_json.json');
+        await firestoreService.restore('./data/parkade_json.json');
         console.log('Update Success');
     }
     catch (error) {
