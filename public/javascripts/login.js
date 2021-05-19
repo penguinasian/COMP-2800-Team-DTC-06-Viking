50d747b0179084560e4fb8abb3df800a613995a9
Daniel Zhang <zhangqiangtianchen@gmail.com>
2021-05-19 11:47:30 -0700

    changed js onclike to change

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -32,6 +32,7 @@ var uiConfig = {
         },
 
     },
+    
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
     signInSuccessUrl: '../home.html',
@@ -39,8 +40,8 @@ var uiConfig = {
         // Leave the lines as is for the providers you want to offer your users.
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
         firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
         //firebase.auth.PhoneAuthProvider.PROVIDER_ID
     ],
