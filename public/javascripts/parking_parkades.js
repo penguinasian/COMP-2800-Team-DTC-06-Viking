19cd4d2dfa46b9c1992116a5cd68e58ea3a8bfae
Jonghoon Jang <jayjang0209@gmail.com>
2021-05-19 10:34:57 -0700

    Create js and constants for parkades page

---
+++ public/javascripts/parking_parkades.js
@@ -0,0 +1,9 @@
+const mapIcons = {
+    "parkade": "./images/parkade.png",
+    "locker": "./images/locker.png",
+    "full": "./images/full.png"
+}
+
+const full = 0;
+const lowAvailablity = 0.3;
+const highAvailablity = 0.6;
