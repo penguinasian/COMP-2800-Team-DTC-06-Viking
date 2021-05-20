2c0052edee3c008643992ab284b20cd9c45f08f8
Daniel Zhang <zhangqiangtianchen@gmail.com>
2021-05-19 21:12:41 -0700

    add pagination for popular routes page

--- public/javascripts/popular_routes.js
+++ public/javascripts/popular_routes.js
@@ -1,3 +1,4 @@
+
 function add_popularRoutes(id, ROUTE_NAME, ROUTE_STATIC_IMG, ROUTE_LENGTH, ROUTE_DIFFICULTY, ROUTE_POPULARITY) {
 
     //create an individule div for each routes inside parent div  
@@ -60,29 +61,131 @@ function add_popularRoutes(id, ROUTE_NAME, ROUTE_STATIC_IMG, ROUTE_LENGTH, ROUTE
     document.getElementsByClassName("routesImage")[0].appendChild(individule_routes)
 }
 
+
 function readPopularRoutes() {
+    
     firebase.auth().onAuthStateChanged(function (user) {
         var user = firebase.auth().currentUser;
         var uid = user.uid;
         db.collection("users").doc(uid)
             .get().then(function (doc) {
-                db.collection("popular_routes")
+                db.collection("popular_routes").orderBy("ROUTE_POPULARITY", "desc").limit(4)
                     .get()
                     .then(function (query) {
+
+                        pageStart = query.docs[0]
+                        pageEnd = query.docs[3]                   
+                        
+
                         query.forEach(function (doc) {
 
                             add_popularRoutes(doc.id, doc.data().ROUTE_NAME, doc.data().ROUTE_STATIC_IMG, doc.data().ROUTE_LENGTH
                                 , doc.data().ROUTE_DIFFICULTY, doc.data().ROUTE_POPULARITY)
-
                         })
                     })
-
+        
             })
 
     })
 }
 
+let pageStart = null;
+let pageEnd = null;
 readPopularRoutes();
+
+
+function AddNextClickPagination() {
+    
+    firebase.auth().onAuthStateChanged(function (user) {
+        paginationButton = document.getElementById("nextButton");
+        paginationButton.addEventListener("click", function () {
+            console.log("button was clicked");
+            firebase.auth().onAuthStateChanged(function (user) {
+                var user = firebase.auth().currentUser;
+                var uid = user.uid;
+                document.getElementById("previousButton").className = ""
+                db.collection("users").doc(uid)
+                    .get().then(function (doc) {
+
+                        db.collection("popular_routes").orderBy("ROUTE_POPULARITY", "desc").startAfter(pageEnd).limit(4)
+                            .get()
+                            .then(function (query) {
+
+                                if (!query.size) {
+                                    return;
+                                }
+
+                                pageStart = query.docs[0]
+                                pageEnd = query.docs[3]
+                               
+                                document.getElementsByClassName("routesImage")[0].innerHTML = "";
+                                
+                                if (!pageEnd) {
+                                    nextButton = document.getElementById("nextButton");
+                                    nextButton.className = "hidden";
+                                }
+                                query.forEach(function (doc) {
+
+                                    add_popularRoutes(doc.id, doc.data().ROUTE_NAME, doc.data().ROUTE_STATIC_IMG, doc.data().ROUTE_LENGTH
+                                        , doc.data().ROUTE_DIFFICULTY, doc.data().ROUTE_POPULARITY)
+                                })
+                            })
+
+                    })
+
+            })
+        })
+    })
+}
+
+AddNextClickPagination();
+
+
+function AddPreviousClickPagination() {
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        paginationButton = document.getElementById("previousButton");
+        paginationButton.addEventListener("click", function () {
+            console.log("button was clicked");
+            firebase.auth().onAuthStateChanged(function (user) {
+                
+                document.getElementById("nextButton").className = ""
+                var user = firebase.auth().currentUser;
+                var uid = user.uid;
+                db.collection("users").doc(uid)
+                    .get().then(function (doc) {
+
+                        db.collection("popular_routes").orderBy("ROUTE_POPULARITY", "desc").endBefore(pageStart).limitToLast(4)
+                            .get()
+                            .then(function (query) {
+                                if (!query.size) {
+                                    return;
+                                }
+                                pageStart = query.docs[0]
+                                pageEnd = query.docs[3]
+                             
+                                document.getElementsByClassName("routesImage")[0].innerHTML = "";
+                           
+                                if (!pageEnd) {
+                                    previousButton = document.getElementById("previousButton");
+                                    previous.className = "hidden";
+                                }
+                                query.forEach(function (doc) {
+
+                                    add_popularRoutes(doc.id, doc.data().ROUTE_NAME, doc.data().ROUTE_STATIC_IMG, doc.data().ROUTE_LENGTH
+                                        , doc.data().ROUTE_DIFFICULTY, doc.data().ROUTE_POPULARITY)
+                                })
+                            })
+
+                    })
+
+            })
+        })
+    })
+}
+
+AddPreviousClickPagination();
+
 
 function addLikeListener(id, likes_number, like_div) {
     firebase.auth().onAuthStateChanged(function (user) {
@@ -109,32 +212,20 @@ function addFilterListenerForLevel() {
 
     var levelDropDown = document.getElementById("levelOption");
     firebase.auth().onAuthStateChanged(function (user) {
-        levelDropDown.addEventListener("change", function () {
+        levelDropDown
+        .addEventListener("change", function () {
             var text = e.value
             console.log("button was clicked")
-            db.collection("popular_routes")
+            db.collection("popular_routes").where("ROUTE_DIFFICULTY", "==", text)
                 .get()
                 .then(function (query) {
                     document.getElementsByClassName("routesImage")[0].innerHTML = "";
                     query.forEach(function (doc) {
-                        if (doc.data().ROUTE_DIFFICULTY == text) {
+                       
                             add_popularRoutes(doc.id, doc.data().ROUTE_NAME, doc.data().ROUTE_STATIC_IMG, doc.data().ROUTE_LENGTH
                                 , doc.data().ROUTE_DIFFICULTY, doc.data().ROUTE_POPULARITY)
 
-                        }
-
-                        if (text == "Level: All") {
-                            db.collection("popular_routes")
-                                .get()
-                                .then(function (query) {
-                                    document.getElementsByClassName("routesImage")[0].innerHTML = "";
-                                    query.forEach(function (doc) {
-                                        add_popularRoutes(doc.id, doc.data().ROUTE_NAME, doc.data().ROUTE_STATIC_IMG, doc.data().ROUTE_LENGTH
-                                            , doc.data().ROUTE_DURATION, doc.data().ROUTE_DIFFICULTY, doc.data().ROUTE_POPULARITY)
-                                    })
-                                })
-
-                        }
+                        
                     })
                 })
         })
@@ -221,7 +312,7 @@ function addFilterListenerForLength() {
     })
 }
 
-addFilterListenerForPopularity();
+addFilterListenerForLength();
 
 function addFilterListenerForPopularity() {
     var e = document.getElementById("popularity")
