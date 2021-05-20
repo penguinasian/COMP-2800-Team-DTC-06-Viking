13ad596b07e7b368c0d177171eba9a10fe805d99
Kevin <kg25000@gmail.com>
2021-05-19 17:51:49 -0700

    Added reservation code

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,9 +1,12 @@
 let bookmarksArray = [];
 let reservationsArray = [];
 let routesList = [];
+const lockersList = new Set();
+let lockerArray = [];
 let bookmarkIndex = 0;
 let bookmarkCount, userName;
 let firstUpdate = false;
+let userID;
 
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
@@ -16,6 +19,7 @@ firebase.auth().onAuthStateChanged(function (user) {
             .then(function (doc) {
                 document.getElementById("profile-title").innerText = doc.data().name + "'s Profile";
                 bookmarksArray = doc.data().bookmarks;
+                userID = doc.data().user_id;
                 console.log(bookmarksArray);
                 bookmarkCount = bookmarksArray.length;
                 console.log(bookmarkCount);
@@ -81,5 +85,72 @@ function decrementBookmarkIndex(decrement) {
         bookmarkIndex = bookmarkCount - 1;
     } else {
         bookmarkIndex--;
+    }
+}
+
+//fetch user reservations from the reservation collection
+
+function fetchReservations() {
+    let allReservations = db.collection("reservation").where("USER_ID", "==", userID);
+        allReservations.get()
+        .then((snapshot) => {
+            snapshot.docs.forEach((doc) => {
+                reservationsArray.push(doc.data());
+            });
+            getLockerID();
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }; 
+
+//adds unique locker ids from reservations to an array
+
+function getLockerID() {
+    reservationsArray.forEach((reservation) => {
+        lockersList.add(reservation.LOCKER_ID);
+    });
+    fetchLockerLocation();
+}
+
+//gets locker info from set of unique locker id
+
+function fetchLockerLocation() {
+    let allLockers = db.collection("lockers");
+    for (let locker of lockersList) {
+        allLockers.where("id", "==", locker).get()
+        .then((snapshot) => {
+            snapshot.docs.forEach((doc) => {
+                lockerArray.push(doc.data());
+            });
+            updateUserReservation();
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }
+}; 
+
+//gets locker address from locker info
+
+function getLockerAddress(lockerID) {
+    lockerArray.forEach((locker) => {
+        if (locker.id == lockerID) {
+            return locker.address;
+        }
+    });
+}
+
+//adds reservation html based on parameters
+
+function createReservationHistory(resID, resAddress, resBox, resDate, resDuration) {
+    document.getElementById("reservations-div").insertAdjacentHTML("beforeend", '<table class="tg"><thead><tr><th class="titles">Locker Address</th><th class="values" id="raddress'+ resID +'">'+ resAddress +'</th></tr><tr><td class="titles">Box #</td><td class="values" id="rbox'+ resID +'">'+ resBox +'</td></tr><tr><td class="titles">Start Date</td><td class="values" id="rdate'+ resID +'">'+ resDate +'</td></tr><tr><td class="titles">Duration</td><td class="values" id="rduration'+ resID +'">'+ resDuration +'</td></tr></thead></table>');
+}
+
+//combines information and outputs it to the page as html
+
+function updateUserReservation() {
+    for (var i = 0; i < reservationsArray.length; i++) {
+        createReservationHistory(i, getLockerAddress(i), reservationsArray[i].BOX_ID, reservationsArray[i].RES_BEGIN, reservationsArray[i].RES_DURATION_WEEKS);
     }
 }
